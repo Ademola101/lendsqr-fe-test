de7c5d72f156415b06dee79e3148da4021931575
Ademola <ademolaogunmokun@yahoo.com>
2022-10-07 22:14:52 +0100

    set the logo part of the homepage

---
+++ src/Pages/Home/PabloLogo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const PabloLogo = () => {
+  return (
+    <div>
+
+      <img src="/images/pablo-sign-in.png" alt="" />
+    </div>
+  );
+};
+
+export default PabloLogo;
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from '../../Components/Logo';
+import PabloLogo from './PabloLogo';
+
+const index = () => {
+  return (
+    <div>
+      <Logo/>
+      <PabloLogo/>
+
+    </div>
+  );
+};
+
+export default index;
