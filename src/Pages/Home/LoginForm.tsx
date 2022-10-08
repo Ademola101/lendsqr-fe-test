a0d4437b55b9bc123bbe7776cb28b891815d7e58
Ademola <ademolaogunmokun@yahoo.com>
2022-10-08 21:31:06 +0100

    destructure and remove the inputLabel for hook-form integration

--- src/Pages/Home/LoginForm.tsx
+++ src/Pages/Home/LoginForm.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
-import InputLabel from './InputLabel';
 import styles from '../../styles/LoginForm.module.css';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+type Data = {
+  email: string;
+  password: string;
+}
 
 const LoginForm = () => {
 
-  const { register, handleSubmit, formState:{ errors } } = useForm();
-  const onSubmit = () => console.log('submit');
+  const onSubmit = (data:Data) => console.log(data);
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  });
 
+  const { register, handleSubmit, formState:{ errors } } = useForm<Data>({
+    resolver: yupResolver(schema),
+  });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
 
-      <div className= {styles.input}>
-        <InputLabel  placeholder="Email"  type = 'email' {...register('email')}   />
-        <InputLabel type='password'  placeholder="Password" {...register('password')}  />
+      <div className= {styles.container}>
+
+        <div>
+          <input className= {styles.input} type="email" placeholder="Email" {...register('email')} />
+          <p className= {styles.error}>{errors.email?.message}</p>
+
+
+        </div>
+
+        <div>
+          <input className= {styles.input} type="password" placeholder="Password" {...register('password')} />
+          <p className= {styles.error}>{errors.password?.message}</p>
+
+        </div>
+
+
+
+
+
+
+
+
+
       </div>
 
       <button type='submit' className={styles.loginButton}>
